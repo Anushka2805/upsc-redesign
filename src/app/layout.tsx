ef85166beddf0262517f95c5ab1753aee890845f
aartimalik-6905 <malikaarti6905@gmail.com>
2025-10-04 19:44:44 +0530

    pushed commits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { SplashScreen } from "@/components/SplashScreen";
 import Navbar from "@/commonComponents/NavBar";
+import ChatbotIcon from "@/commonComponents/ChatbotIcon"; // 1. Import the ChatbotIcon
 
 // Make sure "export default function" is exactly like this
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <SplashScreen />
           <Navbar />
           <main>{children}</main>
+          <ChatbotIcon /> {/* 2. Add the component here */}
         </ThemeProvider>
       </body>
     </html>
